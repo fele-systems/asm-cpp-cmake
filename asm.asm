08458ab1f704878245e1a77c01b16553d946fc99
fele-systems <61920953+fele-systems@users.noreply.github.com>
2024-07-09 11:36:22 -0300

    More functions

--- asm.asm
+++ asm.asm
@@ -1,4 +1,64 @@
+extern from_cpp
+
 section .text
+
+; global from_asm
+global from_asm
+from_asm:
+    call from_cpp
+    ret
+
+
+; uint32_t fl_str_len(const char* src@rdi);
+global fl_str_len
+fl_str_len:
+    mov rax, rdi        ; Copy the start address
+.start_loop:
+    cmp BYTE [rax], 0    ; Check if current char is NULL
+    je .exit_loop       ; If it is NULL, jump to exit
+    inc rax             ; Increment current char
+    jmp .start_loop
+.exit_loop:
+    sub rax, rdi
+    ret
+
+; int32_t fl_str_toupper(char* src@rdi, uint32_t n@rsi, char* dst@rdx);
+global fl_str_toupper
+fl_str_toupper:
+    ; Check for null pointers
+    cmp rdx, 0
+    je .exit_error
+    cmp rdi, 0
+    je .exit_error
+
+    mov r8, 0; Initialize counter
+.loop_condition:
+    cmp r8d, esi    ; Loop while counter is less than n
+    jb .loop_body
+    mov eax, r8d
+    ret
+.loop_body:
+    mov rax, rdi        ; Leads character from src[counter]
+    add rax, r8
+    mov al, BYTE [rax]
+    cmp al, 97          ; Checks if the character is a lower case one
+    jb .loop_body_end
+    cmp al, 122
+    ja .loop_body_end
+
+    sub al, 32
+
+.loop_body_end:
+    mov rbx, rdx        ; Copies the character to dst[counter]
+    add rbx, r8
+    mov BYTE [rbx], al
+    inc r8d
+    jmp .loop_condition
+
+.exit_error:
+    mov eax, -1
+    ret
+
 global add32
 add32:
     add rsi, rdi
@@ -23,6 +83,7 @@ adc32:
     mov eax, edi
     ret
 
+; 
 
 ; bool add64_check_carry(uint64_t,uint64_t,uint64_t*);
 global add64_check_carry
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <cstdint>
 #include <iostream>
-#include <iomanip>
+#include <vector>
+
 extern "C" {
 	uint32_t add32(uint32_t,uint32_t);
 
@@ -8,8 +9,15 @@ extern "C" {
 
     uint32_t adc32(uint32_t,uint32_t,bool*);
 
-
     bool add64_check_carry(uint64_t,uint64_t,uint64_t*);
+
+    uint32_t fl_str_len(const char* src);
+
+    int32_t fl_str_toupper(const char* src, uint32_t n, char* dst);
+
+    void from_cpp() {
+        std::cout << "Calling C++ from Assembly!" << std::endl;
+    }
 }
 
 bool add_check_carry(uint64_t x, uint64_t y, uint64_t& out)
@@ -17,24 +25,31 @@ bool add_check_carry(uint64_t x, uint64_t y, uint64_t& out)
     return add64_check_carry(x, y, &out);
 }
 
-#define dbg(what) std::cout << #what << ": " << what << std::endl;
-#define dbg_hl(hl) std::cout << #hl << "[] = { 0x" << std::hex << hl[0] \
-    << ", 0x" << std::hex << hl[1] << "}" << std::dec << std::endl
-
 int main() {
-    bool carry = false;
-    uint32_t num0[2] = { 0xFFFFFFFF, 0 };
-    dbg_hl(num0);
-    uint32_t num1[2] = { 1, 0 };
-    dbg_hl(num1);
-    uint32_t num2[2] = { 0, 0 };
+	auto sum = add32(256, 100);
 
-    num2[0] = adc32(num0[0], num1[0], &carry);
-    num2[1] = num0[1] + num1[1] + carry;
-    dbg_hl(num2);
-    uint64_t result = static_cast<uint64_t>(num2[0]) | (static_cast<uint64_t>(num2[1]) << 32);
-    uint64_t unsafe_result = *reinterpret_cast<uint64_t*>(num2);
-    dbg(result);
-    dbg(unsafe_result);
+    const char* mystr = "Hello, world";
+    auto l = fl_str_len(mystr);
+    std::cout << "Len of '" << mystr << "' is: " << l << std::endl;
+
+    std::vector<char> buffer(l+1, '\0');
+    auto r = fl_str_toupper(mystr, l, buffer.data());
+    std::cout << "Uppercase: " << std::string{ buffer.data(), r } << " ... Result: " << r << std::endl;
+
+    bool b = false;
+    std::cout << "Before settrue: " << b << std::endl;
+    settrue(&b);
+    std::cout << "After settrue: " << b << std::endl;
+
+    bool carry = false;
+    auto s = adc32(0xFFFFFFFF, 100, &carry);
+
+    uint64_t result;
+
+    if (add_check_carry( UINT64_MAX, 1, result) )
+    {
+        std::cout << "had carry" << std::endl;
+    }
+
     return 0;
 }
